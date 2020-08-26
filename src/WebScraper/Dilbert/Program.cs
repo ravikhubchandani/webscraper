4dd17c91c0b59d8f1a6583b0d4e5f16b9efa1872
Ravi <ravirajkhubchandani@gmail.com>
2020-08-26 14:08:36 +0100

    Creates structure for core library and dilbert test

---
+++ src/WebScraper/Dilbert/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebScraper.Dilbert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/WebScraper/WebScraper.Core/HttpClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebScraper.Core
+{
+    public class HttpClient
+    {
+    }
+}
