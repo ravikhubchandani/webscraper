5b2c80528b945e22501a0bb4c6ad7ce1b8be03fc
Ravi <ravirajkhubchandani@gmail.com>
2020-08-27 13:29:18 +0100

    * includes more strips from other sites * renames dilbert to generic 'daily strips'

--- src/WebScraper/Dilbert/Program.cs
+++ src/WebScraper/Dilbert/Program.cs
@@ -1,34 +1,80 @@
-﻿using System;
+﻿using AngleSharp.Dom;
+using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using WebScraper.Core;
 
-namespace WebScraper.Dilbert
+namespace WebScraper.DailyStrips
 {
     class Program
     {
         static void Main(string[] args)
         {
-            WebSource source = new WebSource("https://dilbert.com/");
-            DateTime when = DateTime.Today;
-            string body = source.GetResponse("strip", when.ToString("yyyy-MM-dd")).Result;
-            HtmlContentParser parser = new HtmlContentParser(body);
-            var dom = parser.GetFirstChild("div.img-comic-container img");
-            var element = dom.FirstOrDefault();
-            if (element != null)
+            var stripCollection = new List<(string src, string title)>();
+            AddDilbert(stripCollection, DateTime.Today);
+            AddGoComicsStrip(stripCollection, DateTime.Today, "garfield", "adult-children", "peanuts", "9to5", "calvinandhobbes", "wtduck", "shen-comix");
+            string msg = GetHtmlNotification(stripCollection);
+            SendEmailNotification(msg);
+        }
+
+        private static void SendEmailNotification(string msg)
+        {
+            var notifier = new EmailNotifier { EmailSubject = "Daily strips - " + DateTime.Today.ToString("dddd dd", CultureInfo.GetCultureInfo("es-ES")) };
+            notifier.AddReciever("ravirajkhubchandani@gmail.com");
+            notifier.Push(msg);
+        }
+
+        private static void AddDilbert(List<(string src, string title)> stripCollection, DateTime when)
+        {
+            string source = "https://dilbert.com/";
+            string selector = "div.img-comic-container img";
+            string[] route = { "strip", when.ToString("yyyy-MM-dd") };
+            IElement domElement = GetDomForStrip(source, selector, route);
+            (string, string) stripData = GetStripData(domElement, "src", "alt");
+            stripCollection.Add(stripData);
+        }
+
+        private static void AddGoComicsStrip(List<(string src, string title)> stripCollection, DateTime when, params string[] codes)
+        {
+            string source = "https://gocomics.com/";
+            string selector = "div.comic__image img.img-fluid";
+            foreach (string code in codes)
             {
-                var notifier = new EmailNotifier { EmailSubject = "Dilbert " + when.ToString("dddd dd", CultureInfo.GetCultureInfo("es-ES"))  };
-                notifier.AddReciever("ravirajkhubchandani@gmail.com");
-                string alt = element.Attributes["alt"].Value;
-                string src = element.Attributes["src"].Value.Trim('/');
-                string msg = GetHtmlNotification(src, alt);
-                notifier.Push(msg);
+                string[] route = { code, when.ToString("yyyy"), when.ToString("MM"), when.ToString("dd") };
+                IElement domElement = GetDomForStrip(source, selector, route);
+                if (domElement != null)
+                {
+                    (string, string) stripData = GetStripData(domElement, "src", "alt");
+                    stripCollection.Add(stripData);
+                }
             }
         }
 
-        private static string GetHtmlNotification(string imgSrc, string imgTitle)
+        private static IElement GetDomForStrip(string source, string selector, string[] route)
         {
-            return $"<html><body><img src='{imgSrc}'><p>{imgTitle}</p></body></html>";
+            WebSource webSource = new WebSource(source);
+            string body = webSource.GetResponse(route).Result;
+            HtmlContentParser parser = new HtmlContentParser(body);
+            var dom = parser.GetFirstChild(selector);
+            return dom.FirstOrDefault();
+        }
+
+        private static (string, string) GetStripData(IElement domElement, string imgAttr, string titleAttr)
+        {            
+            string title = domElement.Attributes[titleAttr]?.Value ?? string.Empty;
+            string img = domElement.Attributes[imgAttr]?.Value?.Trim('/') ?? string.Empty;
+            return (img, title);
+        }
+
+        private static string GetHtmlNotification(List<(string src, string title)> stripData)
+        {
+            return $"<html><body>{string.Join("<hr />", stripData.Select(x => GetHtmlForStrip(x.src, x.title)))}</body></html>";
+        }
+
+        private static string GetHtmlForStrip(string imgSrc, string imgTitle)
+        {
+            return $"<p><i>{imgTitle}</i></p><img src='{imgSrc}' />";
         }
     }
 }
